5c734411fbb07fe7e2af79feb37a431fbb165f33
Mark <76394742+pavorkmert@users.noreply.github.com>
2024-07-05 22:47:47 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
 // next.config.js
-
 module.exports = {
     basePath: 'front5',
     assetPrefix: 'front5',
+    trailingSlash: true,
+    images: {
+      unoptimized: true,
+    },
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        '/': { page: '/' },
+        '/about': { page: '/about' },
+        '/work': { page: '/work' },
+        '/contacts': { page: '/contacts' },
+      };
+    },
   };
   
